b4e82af8f2d8c3d0a60710bb8e1b9cecdf7e35c8
Blessy P Roy <74947416+blessy1234@users.noreply.github.com>
2021-06-03 20:56:33 +0530

    Add files via upload

---
+++ labcycle 4/shape.java
@@ -0,0 +1,17 @@
+package Graphics;
+import Graphics.area;
+public class shape 
+{
+
+	public static void main(String[] args) 
+	{
+		area ar=new area();
+		ar.rectangle();
+		ar.triangle();
+		ar.square();
+		ar.circle();
+	}
+     
+       
+      
+}
