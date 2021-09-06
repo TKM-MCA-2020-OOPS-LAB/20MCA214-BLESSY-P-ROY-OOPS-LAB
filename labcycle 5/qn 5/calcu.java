f04dbf9d83d7fae231d081df39a3e0f8920e9f60
Blessy P Roy <74947416+blessy1234@users.noreply.github.com>
2021-09-06 15:31:28 +0530

    Add files via upload

---
+++ labcycle 5/qn 5/calcu.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/*
+<applet code="calcu" width=300 height=300>
+</applet>
+*/
+public class calcu extends Applet implements ActionListener{
+String msg=" ";
+int v1,v2,result;
+TextField t1;
+Button b[]=new Button[10];
+Button add,sub,mul,div,clear,EQ;
+char OP;
+public void init(){
+Color k=new Color(120,89,90);
+setBackground(k);
+t1=new TextField(100);
+GridLayout gl=new GridLayout(4,5);
+setLayout(gl);
+for(int i=0;i<10;i++){
+b[i]=new Button(""+i);
+}
+add=new Button("+");
+sub=new Button("-");
+mul=new Button("*");
+div=new Button("/");
+clear=new Button("clear");
+EQ=new Button("=");
+t1.addActionListener(this);
+add(t1);
+for(int i=0;i<10;i++) {
+add(b[i]);
+}
+add(add);
+add(sub);
+add(mul);
+add(div);
+add(clear);
+add(EQ);
+for(int i=0;i<10;i++) {
+b[i].addActionListener(this);
+}
+add.addActionListener(this);
+sub.addActionListener(this);
+mul.addActionListener(this);
+div.addActionListener(this);
+clear.addActionListener(this);
+EQ.addActionListener(this);
+}
+public void actionPerformed(ActionEvent ae){
+String str=ae.getActionCommand();
+char ch=str.charAt(0);
+if (Character.isDigit(ch))
+t1.setText(t1.getText()+str);
+else if(str.equals("+"))
+{
+v1=Integer.parseInt(t1.getText());
+OP='+';
+t1.setText("");
+}
+else if(str.equals("-"))
+{
+v1=Integer.parseInt(t1.getText());
+OP='-';
+t1.setText("");
+}
+else if(str.equals("*"))
+{
+v1=Integer.parseInt(t1.getText());
+OP='*';
+t1.setText("");
+}
+else if(str.equals("/"))
+{
+v1=Integer.parseInt(t1.getText());
+OP='/';
+t1.setText("");
+}
+
+if(str.equals("=")){
+v2=Integer.parseInt(t1.getText());
+if(OP=='+')
+result=v1+v2;
+else if(OP=='-')
+result=v1-v2;
+else if(OP=='*')
+result=v1*v2;
+else if(OP=='/')
+result=v1/v2;
+}
+if(str.equals("clear"))
+{
+t1.setText("");
+}
+}
+}
