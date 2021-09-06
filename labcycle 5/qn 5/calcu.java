0a652d11e18bdd068f17cb07f6b3e7873fadf9e1
Blessy P Roy <74947416+blessy1234@users.noreply.github.com>
2021-09-06 15:39:49 +0530

    Update calcu.java

--- labcycle 5/qn 5/calcu.java
+++ labcycle 5/qn 5/calcu.java
@@ -1,97 +1,110 @@
-import java.awt.*;
-import java.awt.event.*;
 import java.applet.*;
-/*
-<applet code="calcu" width=300 height=300>
-</applet>
-*/
-public class calcu extends Applet implements ActionListener{
-String msg=" ";
-int v1,v2,result;
-TextField t1;
-Button b[]=new Button[10];
-Button add,sub,mul,div,clear,EQ;
-char OP;
-public void init(){
-Color k=new Color(120,89,90);
-setBackground(k);
-t1=new TextField(100);
-GridLayout gl=new GridLayout(4,5);
-setLayout(gl);
-for(int i=0;i<10;i++){
-b[i]=new Button(""+i);
-}
-add=new Button("+");
-sub=new Button("-");
-mul=new Button("*");
-div=new Button("/");
-clear=new Button("clear");
-EQ=new Button("=");
-t1.addActionListener(this);
-add(t1);
-for(int i=0;i<10;i++) {
-add(b[i]);
-}
-add(add);
-add(sub);
-add(mul);
-add(div);
-add(clear);
-add(EQ);
-for(int i=0;i<10;i++) {
-b[i].addActionListener(this);
-}
-add.addActionListener(this);
-sub.addActionListener(this);
-mul.addActionListener(this);
-div.addActionListener(this);
-clear.addActionListener(this);
-EQ.addActionListener(this);
-}
-public void actionPerformed(ActionEvent ae){
-String str=ae.getActionCommand();
-char ch=str.charAt(0);
-if (Character.isDigit(ch))
-t1.setText(t1.getText()+str);
-else if(str.equals("+"))
+import java.awt.event.*;
+import java.awt.*;
+//<applet code="calcu" height=300 width=200></applet>
+public class calcu extends Applet implements ActionListener
 {
-v1=Integer.parseInt(t1.getText());
-OP='+';
-t1.setText("");
-}
-else if(str.equals("-"))
-{
-v1=Integer.parseInt(t1.getText());
-OP='-';
-t1.setText("");
-}
-else if(str.equals("*"))
-{
-v1=Integer.parseInt(t1.getText());
-OP='*';
-t1.setText("");
-}
-else if(str.equals("/"))
-{
-v1=Integer.parseInt(t1.getText());
-OP='/';
-t1.setText("");
-}
-
-if(str.equals("=")){
-v2=Integer.parseInt(t1.getText());
-if(OP=='+')
-result=v1+v2;
-else if(OP=='-')
-result=v1-v2;
-else if(OP=='*')
-result=v1*v2;
-else if(OP=='/')
-result=v1/v2;
-}
-if(str.equals("clear"))
-{
-t1.setText("");
-}
-}
+	TextField t;
+	Button b[];
+	Panel p;
+	String c, arr[]={"1","2","3","+","4","5","6","-","7","8","9","*","0",".","=","/"};
+	double n1,n2,ans;
+	public void init()
+	{
+		t= new TextField(20);
+		b=new Button[16];
+		p= new Panel();
+		for(int i=0;i<16;i++)
+			b[i]=new Button(arr[i]);
+		p.setLayout(new GridLayout(4, 4, 10, 10));
+		p.setSize(700,1500);
+		
+	}
+	public void start()
+	{
+		add(t);
+		for(int i=0;i<16;i++)
+		{
+			p.add(b[i]);
+			b[i].addActionListener(this);
+		}
+		add(p);
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		switch (ae.getActionCommand())
+		{
+			case "1": insert("1"); break;
+			case "2": insert("2"); break;
+			case "3": insert("3"); break;
+			case "4": insert("4"); break;
+			case "5": insert("5"); break;
+			case "6": insert("6"); break;
+			case "7": insert("7"); break;
+			case "8": insert("8"); break;
+			case "9": insert("9"); break;
+			case "0": insert("0"); break;
+			case ".": insert("."); break;
+			case "+": addition(); break;
+			case "-": substract(); break;
+			case "*": multiply(); break;
+			case "/": divide(); break;
+			case "=": cal(); break;
+		}
+	}
+	void insert(String n)
+	{
+		if (t.getText().equals(""))
+				t.setText(n);
+			else
+				t.setText(t.getText()+n);
+	}
+	void addition()
+	{
+		n1=Double.parseDouble(t.getText());
+		t.setText(t.getText()+"+");
+		c="+";
+	}
+	void substract()
+	{
+		n1=Double.parseDouble(t.getText());
+		t.setText(t.getText()+"-");
+		c="-";
+	}
+	void multiply()
+	{
+		n1=Double.parseDouble(t.getText());
+		t.setText(t.getText()+"*");
+		c="*";
+	}
+	void divide()
+	{
+		n1=Double.parseDouble(t.getText());
+		t.setText(t.getText()+"/");
+		c="/";
+	}
+	void cal()
+	{
+		n2=Double.parseDouble(t.getText().substring(t.getText().indexOf(c)+1,t.getText().length()));
+		if(c.equals("+"))
+		{
+			t.setText(Double.toString(n1+n2));
+			n1=n1+n2;
+		}
+		if(c.equals("-"))
+		{
+			t.setText(Double.toString(n1-n2));
+			n1=n1-n2;
+		}
+		if(c.equals("*"))
+		{
+			t.setText(Double.toString(n1*n2));
+			n1=n1*n2;
+		}
+		if(c.equals("/"))
+		{
+			t.setText(Double.toString(n1/n2));
+			n1=n1/n2;
+		}
+	}
 }
